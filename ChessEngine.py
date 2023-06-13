272a8e31d0c79bc6df68120a9c87c7ad2149df0d
Your Name <you@example.com>
2023-06-13 17:33:22 +0530

    Code to draw chess board with pieces

---
+++ ChessEngine.py
@@ -0,0 +1,23 @@
+# File for determining valid moves etc, stores info about the current state of a chess game.
+
+
+class GameState:
+
+    # "--" represents an empty space with no piece
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+
+        self.whiteToMove = True
+        self.moveLog = []
+
+
+
---
+++ ChessMain.py
@@ -0,0 +1,92 @@
+"""
+file for user input
+"""
+import pygame as p
+from ChessEngine import GameState
+
+WIDTH = HEIGHT = 512  # 400 is another option
+DIMENSIONS = 8  # dimension of  a chess board are 8x8
+SQ_SIZE = HEIGHT // DIMENSIONS
+MAX_FPS = 15
+IMAGES = {}
+
+"""    
+ Initialize a global dictionary of images. This will be called exactly once in main
+"""
+
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+"""
+The main  driver for our code. This will handle user input and update the graphics
+"""
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = GameState()
+    # print(gs.board)
+
+    loadImages()
+
+    running = True
+
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+'''
+Responsible for all the graphics withing a current game states 
+'''
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)  # draw Squares on the board
+    # add in pieces highlighting or move suggestions
+    drawPieces(screen, gs.board)  # to draw pieces on top of those squares
+
+
+'''
+Draw the squares on  the board. The top left square is always  light
+'''
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+'''
+Draw the pieces on the board using the current GameState.board
+'''
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            piece = board[r][c]
+
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == '__main__':
+    main()
