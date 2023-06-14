67346cc8b595b53116e4bf1328dc1408a223d829
Your Name <you@example.com>
2023-06-14 23:40:04 +0530

    Code for King moves and code  optimization

--- ChessEngine.py
+++ ChessEngine.py
@@ -172,7 +172,15 @@ class GameState:
         '''
 
     def getKingMoves(self, r, c, moves):
-        pass
+        kingMoves = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        allyColor = "w" if self.whiteToMove else "b"
+        for i in range(8):
+            endRow = r + kingMoves[i][0]
+            endCol = c + kingMoves[i][1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor:
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
 
 
 class Move:
